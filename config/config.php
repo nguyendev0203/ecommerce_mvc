6349420c833145ed01022a071921bd3a0a2a9c10
nguyendev0203 <57607759+nguyendev0203@users.noreply.github.com>
2020-09-23 07:11:22 +0700

    23/9 3am

--- config/config.php
+++ config/config.php
@@ -1,3 +1,11 @@
 <?php
 
+define('DEBUG',true);
+
 define('DEFAULT_CONTROLLER', 'Home');
+define('DEFAULT_LAYOUT', 'default');
+
+define('PROOT', '/mvc/'); //set this to for a live server.
+
+define('SITE_TITLE', 'MVC Framework');
+
